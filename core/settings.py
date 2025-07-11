b75cd1d967766a18f61450b185af509428a574c3
Luqmanmh <luqmanmh16@gmail.com>
2025-07-11 22:20:22 +0700

    static fix try1

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ cloudinary.config(
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'API_SECRET': os.getenv('   '),
 }
 
 # Media files (uploads) - use Cloudinary
