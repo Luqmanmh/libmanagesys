8894f72af14d40e75f96aa7fdb300f135a9ecd52
Luqmanmh <luqmanmh16@gmail.com>
2025-07-11 21:03:33 +0700

    try railway fix1

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -16,6 +15,7 @@ SECRET_KEY = 'your-secret-here'
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://perpussidomukti.up.railway.app", "localhost", "127.0.0.1"]
 
 TAILWIND_APP_NAME = 'theme'
 
@@ -107,7 +107,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
