4fa3617bc493775235b88afc16e50c5b3484538d
Luqmanmh <luqmanmh16@gmail.com>
2025-07-12 20:01:34 +0700

    fix text spacing

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://perpussidomukti.up.railway.app", "http://localhost", "http://127.0.0.1"]
