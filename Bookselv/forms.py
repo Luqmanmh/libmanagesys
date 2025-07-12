26a9193eaf0a3787746737416557fef8dbfb43a0
Luqmanmh <luqmanmh16@gmail.com>
2025-07-12 12:29:02 +0700

    change form and model reqs

--- Bookselv/forms.py
+++ Bookselv/forms.py
@@ -4,12 +4,12 @@ from .models import Book, Folder
 class FileUploadForm(forms.Form):
     title = forms.CharField(max_length=255, label="Judul")
     writer = forms.CharField(max_length=255, label="Penulis")
-    publisher = forms.CharField(max_length=255, label="Penerbit")
-    ddc = forms.CharField(max_length=10, label="DDC")
-    count = forms.CharField(max_length=128, label="Jumlah")
-    synopsis = forms.CharField(widget=forms.Textarea, label="Sinopsis")
-    folder = forms.ChoiceField(choices=[], label="Kelas Buku")
-    file = forms.FileField(label="Gambar")
+    publisher = forms.CharField(max_length=255, label="Penerbit", required=False)
+    ddc = forms.CharField(max_length=10, label="DDC", required=False)
+    count = forms.CharField(max_length=128, label="Jumlah", required=False)
+    synopsis = forms.CharField(widget=forms.Textarea, label="Sinopsis", required=False)
+    folder = forms.ChoiceField(choices=[], label="Kelas Buku", required=False)
+    file = forms.FileField(label="Gambar", required=False)
 
     def __init__(self, *args, **kwargs):
         super(FileUploadForm, self).__init__(*args, **kwargs)
